4fba17be785d501c312f879e4a6b61cb9bb70ed8
Binh Pham <binh.pham@gooddata.com>
2022-08-24 10:57:56 +0700

    Dont use TLSv1_3 when using cert based auth Python2 is not handling well TLS1.3

--- jenkins/__init__.py
+++ jenkins/__init__.py
@@ -56,15 +56,36 @@ import time
 import warnings
 
 import multi_key_dict
+import ssl
 import requests
 import requests.exceptions as req_exc
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
 from six.moves.http_client import BadStatusLine
 from six.moves.urllib.error import URLError
 from six.moves.urllib.parse import quote, urlencode, urljoin, urlparse
 import xml.etree.ElementTree as ET
 
 from jenkins import plugins
+
+CIPHERS = (
+    'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384'
+    ':ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:AES256-SHA'
+)
+
+class TlsAdapter(HTTPAdapter):
+
+    def __init__(self, ssl_options=0, **kwargs):
+        self.ssl_options = ssl_options
+        super(TlsAdapter, self).__init__(**kwargs)
+
+    def init_poolmanager(self, *pool_args, **pool_kwargs):
+        ctx = ssl_.create_urllib3_context(ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+        self.poolmanager = PoolManager(*pool_args,
+                                       ssl_context=ctx,
+                                       **pool_kwargs)
 
 try:
     CLIENT_CERT = os.getenv('CLIENT_CERT')
@@ -73,11 +94,13 @@ try:
     JENKINS_URL = os.getenv('JENKINS_URL', "")
     matched = re.search("https://localhost", JENKINS_URL)
 
-    session = requests.Session()
     if matched:
         CLIENT_CERT = ""
         CA_BUNDLE = False
 
+    session = requests.Session()
+    adapter = TlsAdapter(ssl.OP_NO_TLSv1_3)
+    session.mount("https://", adapter)
     session.cert = (CLIENT_CERT, CLIENT_KEY)
     session.verify = False
 except ImportError:
